75eb776696506ea4cfc95acfbb74f27648df6953
xXiryaXx <masawalicht01@gmail.com>
2025-03-29 21:00:52 -0600

    Se establecio conexion con la base de datos

---
+++ config/conexion.php
@@ -0,0 +1,13 @@
+<?php
+$host = "localhost";
+$dbname = "parque_recreativo";
+$user = "root";
+$password = "";
+
+try {
+    $conexion = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
+    $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Error de conexión: " . $e->getMessage());
+}
+?>
